5c480367663ac89ccc011dd1cd55bd5fc594b67a
Seto Balian <seto.balian@gmail.com>
2024-11-30 15:24:10 +0000

    Use click for setup utility

--- good_morning.py
+++ good_morning.py
@@ -1,5 +1,7 @@
 import pathlib
 
+import click
+
 SOLUTION = """\
 import pathlib
 
@@ -21,20 +23,22 @@ if __name__ == "__main__":
 """
 
 
-def main() -> None:
-    days = (day.name for day in pathlib.Path("solutions").glob("*"))
+@click.command()
+def setup() -> None:
+    solutions_dir = pathlib.Path("solutions")
+    days = (day.name for day in solutions_dir.glob("*"))
     latest = max((int(day[3:]) for day in days if day != ".gitkeep"), default=0)
     if latest == 25:
-        print("Merry Christmas!")
+        click.echo("Merry Christmas!")
         return
     next_ = "day" + f"{latest + 1}".zfill(2)
-    dir_ = pathlib.Path(f"solutions/{next_}")
+    dir_ = pathlib.Path(solutions_dir / next_)
     dir_.mkdir()
     (dir_ / "solution.py").write_text(SOLUTION)
     for name in ["input.txt", "example.txt"]:
         (dir_ / name).open("a").close()
-    print("Good luck!")
+    click.echo("Good luck!")
 
 
 if __name__ == "__main__":
-    main()
+    setup()
