6113ad73062b38584ae70d8384d2d30e1532bda7
Seto Balian <seto.balian@gmail.com>
2025-01-06 15:06:39 +0400

    Day 16 parts 1 and 2

--- solutions/day16/solution.py
+++ solutions/day16/solution.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from collections import defaultdict, deque
 from dataclasses import dataclass, field
 from heapq import heappop, heappush
 from pathlib import Path
@@ -77,49 +78,67 @@ class Maze:
                 raise RuntimeError("unhandled state")
         return [state for state in states if self[state.position] != "#"]
 
-    def find_min_cost(self, start: State) -> int:
+    @staticmethod
+    def num_states(start: State, previous: defaultdict[State, list[State]]) -> int:
+        visited = set[State]([start])
+        queue = deque[State]([start])
+        while queue:
+            node = queue.popleft()
+            for neighbor in previous[node]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append(neighbor)
+        return len(set(node.position for node in visited))
+
+    def solve(self) -> tuple[int, int]:
+        start, end = self.find_start_end()
+        start = State(position=start, velocity=Vector(0, 1))
         queue: list[MazePath] = []
         heappush(queue, MazePath(head=start, cost=0))
-        costs: dict[State, int] = {start: 0}
+        costs: dict[State, int] = {}
+        previous = defaultdict[State, list[State]](list)
         while queue:
             path = heappop(queue)
             node = path.head
-            if self[node.position] == "E":
-                return path.cost
-            else:
-                for neighbor in self.get_neighbors(node):
-                    if node.velocity != neighbor.velocity:
-                        cost = path.cost + 1001
-                    else:
-                        cost = path.cost + 1
-                    if cost < costs.get(neighbor, INF):
-                        heappush(queue, MazePath(neighbor, cost))
-                        costs[neighbor] = cost
-        raise RuntimeError("end not reached")
+            for neighbor in self.get_neighbors(node):
+                if node.velocity != neighbor.velocity:
+                    cost = path.cost + 1001
+                else:
+                    cost = path.cost + 1
+                if cost <= costs.get(neighbor, INF):
+                    heappush(queue, MazePath(neighbor, cost))
+                    costs[neighbor] = cost
+                    previous[neighbor].append(node)
+        best_final_state, best_cost = min(
+            [(node, cost) for node, cost in costs.items() if node.position == end],
+            key=lambda item: item[1],
+        )
+        return best_cost, self.num_states(best_final_state, previous)
 
-    def find_start(self) -> Vector:
+    def find_start_end(self) -> tuple[Vector, Vector]:
+        start, end = None, None
         for i in range(self.rows):
             for j in range(self.cols):
                 if self[Vector(i, j)] == "S":
-                    return Vector(i, j)
+                    start = Vector(i, j)
+                elif self[Vector(i, j)] == "E":
+                    end = Vector(i, j)
+                if start is not None and end is not None:
+                    return start, end
         raise ValueError("start not found")
-
-    def part1(self):
-        start = self.find_start()
-        return self.find_min_cost(State(position=start, velocity=Vector(0, 1)))
 
 
 def main() -> None:
     maze = Maze.from_file(Path("example1.txt"))
-    assert maze.part1() == 7036
+    assert maze.solve() == (7036, 45)
     maze = Maze.from_file(Path("example2.txt"))
-    assert maze.part1() == 11048
+    assert maze.solve() == (11048, 64)
     maze = Maze.from_file(Path("example3.txt"))
-    assert maze.part1() == 1006
+    assert maze.solve() == (1006, 7)
     maze = Maze.from_file(Path("example4.txt"))
-    assert maze.part1() == 1025
+    assert maze.solve() == (1025, 26)
     maze = Maze.from_file(Path("input.txt"))
-    assert maze.part1() == 98416
+    assert maze.solve() == (98416, 471)
     print("All tests passed.")
 
 
